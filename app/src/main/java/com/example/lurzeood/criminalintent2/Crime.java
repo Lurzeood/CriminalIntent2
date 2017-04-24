0a677c12f1562318c035b9baca1f636c5703ed0d
xpc <501929949@qq.com>
2017-04-24 22:00:17 +0800

    配置项目

---
+++ app/src/main/java/com/example/lurzeood/criminalintent2/Crime.java
@@ -0,0 +1,49 @@
+package com.example.lurzeood.criminalintent2;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Lurzeood on 2017/4/24 0024.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
